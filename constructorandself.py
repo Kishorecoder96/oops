3314e1413bc132c2b52a7f4b5fe3b08e11daa4de
Kishorecoder96 <kishorevg82@gmail.com>
2023-08-12 14:33:55 +0530

    add files

---
+++ constructorandself.py
@@ -0,0 +1,11 @@
+class work:
+    def __init__(self,help):     #here we use __init__ is a constructor it will execute the code auto matically 
+        print("everything will be fine") #self keyword used tp give values to variables
+        self.help=help
+    def __init__(self):
+        print("work will bw done")
+        print("these tings will be okay")
+    def __init_(self):
+        print("ejhvbkjsadjzajfnh")
+        print('knskrhgskhlrlsr')
+a=work()
